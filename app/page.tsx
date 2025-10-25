ea9f14275321dc446cab673b70d282a01ebed02c
daman599 <damanpreetk117@gmail.com>
2025-10-25 23:44:39 +0530

    smooth hover animation added

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,13 @@
-import ShootingStarsCard from "@/ui-components/ShootingStarsCard";
+import HoverCard from "@/ui-components/HoverCard";
 
 export default function Home() {
   return (
     <div className="flex items-center justify-center p-8 pb-20 sm:p-50">
-      <ShootingStarsCard
-        text={"Hi there!"} />
+      <HoverCard
+        text={"Hi there!"}
+        color2={"#c77dff"}
+        color1={"#3c096c"}
+      />
     </div>
   );
 }
---
+++ ui-components/HoverCard.tsx
@@ -0,0 +1,44 @@
+"use client";
+import { motion } from "motion/react";
+
+interface PropsType {
+    text: string;
+    color1?: string;
+    color2?: string;
+    textColor?: string;
+}
+
+export default function HoverCard({
+    text,
+    color1 = "#BEBFED",
+    color2 = "#3538CF",
+    textColor = "white",
+}: PropsType) {
+    return (
+        <div className="relative w-[20rem] h-[20rem] rounded-4xl p-2 overflow-hidden cursor-default">
+
+            {/* Background Gradient */}
+            <div style={{ background: `linear-gradient(to bottom, ${color1}, ${color2})` }}
+                className={`absolute inset-0 rounded-4xl`}>
+            </div>
+
+            {/* Diagonal Line Overlay */}
+            <motion.div
+                initial={{ opacity: 0 }}
+                whileHover={{
+                    opacity: 0.5, // fade in effect
+                }}
+                transition={{
+                    duration: 0.8,
+                    ease: "linear",
+                }}
+                className="absolute inset-0 bg-[repeating-linear-gradient(45deg,rgba(255,255,255,0.3),rgba(255,255,255,0.3)_1px,transparent_1px,transparent_40px)] rounded-4xl"
+            ></motion.div>
+
+            {/* Content */}
+            <div className={`relative text-2xl font-normal text-[${textColor}] p-6 z-20`}>
+                {text}
+            </div>
+        </div>
+    );
+}
