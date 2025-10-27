ce7e23f4b9ecfa7a20339152aad27ce96931d269
daman599 <damanpreetk117@gmail.com>
2025-10-28 01:32:53 +0530

    shadow added

--- ui-components/HoverButton.tsx
+++ ui-components/HoverButton.tsx
@@ -3,7 +3,7 @@ import { ArrowRight } from "lucide-react";
 
 export default function HoverButton() {
     return (
-        <button className="reative group flex items-center gap-2 p-2 px-6 border-2 border-[#e7e7e7] rounded-full overflow-hidden cursor-pointer">
+        <button className="reative group flex items-center gap-2 p-2 px-6 border-2 border-[#e7e7e7] rounded-full overflow-hidden cursor-pointer shadow-xl">
             <div className="bg-black w-2 h-2 rounded-full group-hover:scale-[40] transition-all duration-500"></div>
             <span className="text-xl font-semibold group-hover:translate-x-12 transition-all duration-500">Hover Me</span>
 
