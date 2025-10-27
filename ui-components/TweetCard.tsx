046f40ed46e5d65dbc64c2612b314a26a4602b3e
daman599 <damanpreetk117@gmail.com>
2025-10-27 22:51:23 +0530

    subtle animation added

--- ui-components/TweetCard.tsx
+++ ui-components/TweetCard.tsx
@@ -1,10 +1,14 @@
 "use client"
 
+import { motion } from "motion/react";
 import Image from "next/image";
 
 export default function TweetCard() {
     return (
-        <div className="w-[30rem] h-[25rem] p-5 bg-white border-1 border-[#dadada] rounded-xl shadow-md hover:shadow-lg transition-all duration-300">
+        <motion.div initial={{ opacity: 0, y: 2 }}
+            whileInView={{ opacity: 1, y: 0 }}
+            transition={{ duration: 0.8, ease: "easeIn" }}
+            className="w-[30rem] h-[25rem] p-5 bg-white border-1 border-[#dadada] rounded-xl shadow-md hover:shadow-lg transition-all duration-300" >
 
             <div className="flex items-center justify-between">
                 <div className="flex items-center gap-2">
@@ -45,6 +49,6 @@ export default function TweetCard() {
             <div className="flex-1 rounded-xl object-cover mt-2">
                 <Image width={300} height={300} alt="Portfolio" src="/Portfolio.png" className=" h-full w-full object-cover rounded-xl" />
             </div>
-        </div >
+        </motion.div >
     );
 }
