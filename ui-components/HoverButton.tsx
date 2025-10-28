70f252d8580c26c0694b020c299b2cd069d93e84
daman599 <damanpreetk117@gmail.com>
2025-10-28 09:15:04 +0530

    few fixes

--- ui-components/HoverButton.tsx
+++ ui-components/HoverButton.tsx
@@ -5,7 +5,7 @@ export default function HoverButton() {
     return (
         <button className="reative group flex items-center gap-2 p-2 px-6 border-2 border-[#e7e7e7] rounded-full overflow-hidden cursor-pointer shadow-xl">
             <div className="bg-black w-2 h-2 rounded-full group-hover:scale-[40] transition-all duration-500"></div>
-            <span className="text-xl font-semibold group-hover:translate-x-12 transition-all duration-500">Hover Me</span>
+            <span className="text-xl font-semibold group-hover:translate-x-12 group-hover:opacity-0 transition-all duration-500">Hover Me</span>
 
             <div className="absolute text-white text-xl flex items-center gap-2 p-2 font-semibold transition-all duration-500 opacity-0 group-hover:-translate-x-4 group-hover:opacity-100">
                 <span>Hover Me</span>
