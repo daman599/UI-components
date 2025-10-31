e6e8a6e913950f30c5d8251868c2cb4a1bee3459
daman599 <damanpreetk117@gmail.com>
2025-10-31 21:10:05 +0530

    smooth scale out animation added

--- ui-components/CreateNewButton.tsx
+++ ui-components/CreateNewButton.tsx
@@ -11,7 +11,7 @@ import {
     NotebookPen,
     ScrollText,
 } from "lucide-react";
-import { motion } from "motion/react";
+import { AnimatePresence, motion } from "motion/react";
 
 interface ContentType {
     title: string;
@@ -47,39 +47,47 @@ export default function CreateNewButton() {
                 </motion.button>
             )}
 
-            {btnClicked && (
-                <motion.div
-                    initial={{ opacity: 0, y: 10 }}
-                    animate={{ opacity: 1, y: 0 }}
-                    transition={{ duration: 0.3, ease: "easeInOut" }}
-                    className="absolute top-1/1 -translate-y-1/2 w-[20rem] h-[20rem] border-2 border-[#c6c6c6b4] bg-[#ebebeb95] rounded-3xl flex flex-col overflow-hidden shadow-lg"
-                >
-                    <div className="flex items-center justify-between p-3 px-6">
-                        <p className="text-xl text-[#7c7c7cb4]">Create New</p>
-                        <CircleX
-                            onClick={() => setBtnClicked(false)}
-                            className="cursor-pointer"
-                        />
-                    </div>
-
-                    <div className="bg-white flex-1 rounded-t-3xl p-5">
-                        <div className="grid grid-cols-3 gap-4 place-items-center">
-                            {content.map((item, i) => {
-                                const Icon = item.icon;
-                                return (
-                                    <div
-                                        key={i}
-                                        className="flex flex-col items-center justify-center gap-2 w-[6rem] h-[6rem] hover:bg-[#ebebeb95] rounded-2xl p-3 transition"
-                                    >
-                                        <Icon color="#7c7c7cb4" size={30} />
-                                        <span className="text-[#5f5f5fb4]">{item.title}</span>
-                                    </div>
-                                );
-                            })}
+            <AnimatePresence>
+                {btnClicked && (
+                    <motion.div
+                        initial={{ opacity: 0, scale: 0 }}
+                        animate={{ opacity: 1, scale: 1 }}
+                        exit={{ scale: 0, opacity: 0 }}
+                        transition={{
+                            type: "spring",
+                            stiffness: 200,
+                            damping: 20,
+                        }}
+                        className=" top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2
+                        origin-center absolute inset-0 w-[20rem] h-[20rem] border-2 border-[#c6c6c6b4] bg-[#ebebeb95] rounded-3xl flex flex-col overflow-hidden shadow-lg"
+                    >
+                        <div className="flex items-center justify-between p-3 px-6">
+                            <p className="text-xl text-[#7c7c7cb4]">Create New</p>
+                            <CircleX
+                                onClick={() => setBtnClicked(false)}
+                                className="cursor-pointer"
+                            />
                         </div>
-                    </div>
-                </motion.div>
-            )}
-        </div>
+
+                        <div className="bg-white flex-1 rounded-t-3xl p-5">
+                            <div className="grid grid-cols-3 gap-4 place-items-center">
+                                {content.map((item, i) => {
+                                    const Icon = item.icon;
+                                    return (
+                                        <div
+                                            key={i}
+                                            className="flex flex-col items-center justify-center gap-2 w-[6rem] h-[6rem] hover:bg-[#ebebeb95] rounded-2xl p-3 transition"
+                                        >
+                                            <Icon color="#7c7c7cb4" size={30} />
+                                            <span className="text-[#5f5f5fb4]">{item.title}</span>
+                                        </div>
+                                    );
+                                })}
+                            </div>
+                        </div>
+                    </motion.div>
+                )}
+            </AnimatePresence>
+        </div >
     );
 }
