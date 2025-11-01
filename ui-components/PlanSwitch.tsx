3c4fd55a569a24d5ad7c66df3a835373f3f4fce9
daman599 <damanpreetk117@gmail.com>
2025-11-01 13:18:51 +0530

    made color transition smooth

--- ui-components/PlanSwitch.tsx
+++ ui-components/PlanSwitch.tsx
@@ -8,10 +8,10 @@ export default function PlanSwitch() {
     const [plan, setPlan] = useState<"Free" | "Premium">("Free");
 
     return (
-        <div className=" flex items-center w-[28rem] h-[4rem] bg-white border-2 border-[#cfcfcf] rounded-full overflow-hidden
-        shadow-lg backdrop-blur-3xl">
-
-            <motion.div className="absolute top-0 left-0 w-1/2 h-full bg-black rounded-full"
+        <div className=" relative flex items-center w-[28rem] h-[4rem] bg-white border-2 border-[#cfcfcf] rounded-full overflow-hidden
+        backdrop-blur-3xl shadow-[inset_0_2px_4px_rgba(255,255,255,0.6),inset_0_-2px_4px_rgba(0,0,0,0.1),0_6px_20px_rgba(0,0,0,0.25)]
+        ">
+            <motion.div className="absolute top-0 left-0 w-1/2 h-full bg-black rounded-full "
                 animate={{ x: plan === "Premium" ? "100%" : "0%" }}
                 transition={{
                     type: "spring",
@@ -20,23 +20,23 @@ export default function PlanSwitch() {
                 }}>
             </motion.div>
 
-            <div className="relative flex w-full z-10">
+            <div className="relative flex items-center w-full z-10 font-semibold text-center text-xl p-2">
                 <button className={`w-[14rem] h-[4rem] ${plan === "Free" ? " text-white" : " text-black"}
-              rounded-full cursor-pointer`}
+              rounded-full cursor-pointer transition-colors duration-300`}
                     onClick={() => {
                         setPlan("Free");
                     }}
                 >
-                    <span className="font-medium text-center text-xl">Free</span>
+                    <span>Free</span>
                 </button>
 
-                <button className={`w-[14rem] h-[4rem] rounded-full
+                <button className={`w-[14rem] h-[4rem] rounded-full transition-colors duration-300
              cursor-pointer ${plan === "Premium" ? " text-white" : " text-black"}  `}
                     onClick={() => {
                         setPlan("Premium");
                     }}
                 >
-                    <span className="font-medium text-center text-xl" >Premium</span>
+                    <span>Premium</span>
                 </button>
             </div>
         </div >
