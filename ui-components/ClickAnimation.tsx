75198c3057e195d9e89184dc7b91df72a923d4c6
daman599 <damanpreetk117@gmail.com>
2025-11-03 00:36:00 +0530

    smooth animations added

--- ui-components/ClickAnimation.tsx
+++ ui-components/ClickAnimation.tsx
@@ -50,32 +50,49 @@ export default function ClickAnimation() {
     ]
 
     return (
-        <div className="w-100 h-fit my-auto shadow-lg border-2 flex flex-col gap-6 border-[#e8e8e8] rounded-2xl bg-white p-2 px-2 py-3">
+        <div className="w-100 my-auto shadow-lg border-2 flex flex-col gap-6 border-[#e8e8e8] rounded-2xl bg-white p-2 px-2 py-3">
 
             {items.map((item, i) => {
                 const Icon = item.icon;
                 const isOpen = openBtnIndex === i;
 
                 return (
-                    <button
+                    <motion.div
+                        transition={{ duration: 1, ease: "easeOut" }}
+                        key={i}
                         onClick={() => {
                             setOpenBtnIndex(i);
                         }}
-                        key={i}
-                        className="w-full cursor-pointer flex items-center gap-3 px-3 py-1" >
-                        <Icon color={isOpen ? "#5d5d5d" : "#a3a3a3"} className="transition-colors duration-300" />
+                    >
+                        <div
+                            className="w-full cursor-pointer flex items-center gap-3 px-3 py-1" >
+                            <Icon color={isOpen ? "#5d5d5d" : "#a3a3a3"} className="transition-colors duration-300" />
 
-                        <div className="w-full flex items-center justify-between">
-                            <span className="text-xl font-semibold">{item.title}</span>
+                            <div className="w-full flex items-center justify-between">
+                                <span className="text-xl font-semibold">{item.title}</span>
 
-                            <motion.div
-                                animate={{ rotate: isOpen ? 180 : 0 }}
-                                transition={{ duration: 0.3, ease: "easeInOut" }}
-                            >
-                                <ChevronDown color={"#a3a3a3"} size={30} />
-                            </motion.div>
+                                <motion.div
+                                    animate={{ rotate: isOpen ? 180 : 0 }}
+                                    transition={{ duration: 0.3, ease: "easeInOut" }}
+                                >
+                                    <ChevronDown color={"#a3a3a3"} size={30} />
+                                </motion.div>
+                            </div>
                         </div>
-                    </button>
+
+                        <AnimatePresence>
+                            {isOpen && <motion.p
+                                initial={{ opacity: 0, y: -10 }}
+                                animate={{ opacity: 1, y: 0 }}
+                                transition={{ duration: 0.4 }}
+                                exit={{ opacity: 0, y: 10 }}
+                                className="relative z-10 px-4 pb-3 text-gray-600"
+                            >
+                                {item.description}
+                            </motion.p>}
+                        </AnimatePresence>
+
+                    </motion.div>
                 );
             })}
         </div >
