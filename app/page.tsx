140030e424f4468e4f8ad790ea68e6cf9d917e75
daman599 <damanpreetk117@gmail.com>
2025-11-06 13:54:37 +0530

    fixed height to h-screen

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,6 @@
-import CreateNewButton from "@/ui-components/CreateNewButton";
-
 export default function Home() {
   return (
-    <div className="flex items-center justify-center p-8 pb-20 sm:p-50">
-      <CreateNewButton />
+    <div className="flex items-center justify-center h-screen p-8 pb-20 sm:p-50 overflow-hidden">
     </div >
   );
 }
