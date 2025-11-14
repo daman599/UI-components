aa80abe9b4194eb44c47cd951411137c5500abd5
daman599 <damanpreetk117@gmail.com>
2025-11-14 12:21:24 +0530

    few fixes

--- ui-components/RadiatingAvatar.tsx
+++ ui-components/RadiatingAvatar.tsx
@@ -8,22 +8,18 @@ export default function RadiatingAvatar() {
         <div className="relative flex flex-col items-center justify-center">
 
             <motion.div
-                animate={{ scale: [1, 1.3, 1], opacity: [1, 0, 1] }}
-                transition={{ repeat: Infinity, ease: "easeInOut", duration: 3 }}
-                className="absolute w-64 h-64 rounded-full bg-[#727273d1]">
+                animate={{ scale: [1, 1.2, 1], opacity: [1, 0, 1] }}
+                transition={{ repeat: Infinity, ease: "easeInOut", duration: 4 }}
+                className="absolute w-60 h-60 rounded-full bg-[#727273d1]">
             </motion.div>
 
             <motion.div
-                animate={{ scale: [1, 1.2, 1], opacity: [1, 0, 1] }}
-                transition={{ repeat: Infinity, ease: "easeInOut", duration: 3, delay: 0.5 }}
+                animate={{ scale: [1, 1.13, 1], opacity: [1, 0, 1] }}
+                transition={{ repeat: Infinity, ease: "easeInOut", duration: 3, delay: 1 }}
                 className="absolute w-56 h-56 rounded-full bg-[#4f4f50]">
             </motion.div>
 
-            <motion.div
-                animate={{ scale: [1, 1.1, 1], opacity: [1, 0, 1] }}
-                transition={{ repeat: Infinity, ease: "easeInOut", duration: 2, delay: 1 }}
-                className="absolute w-48 h-48 rounded-full bg-[#37393b]">
-            </motion.div>
+            <div className="absolute w-48 h-48 rounded-full bg-[#37393b]"></div>
 
             <Image
                 src={"/Profile-pic.png"}
