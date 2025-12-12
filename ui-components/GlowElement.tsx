1308d79abbc8b94afb1deed15edcca3dfaab389d
daman599 <damanpreetk117@gmail.com>
2025-12-12 14:13:41 +0530

    new glow component added

---
+++ ui-components/GlowElement.tsx
@@ -0,0 +1,14 @@
+export default function GlowElement() {
+    return (
+        <div className="relative group flex items-center">
+            <div className="absolute bg-blue-400 m-auto inset-0 rounded-full blur-md w-24 h-8
+                            group-hover:scale-200 transition duration-500" />
+
+            <div className="relative shadow-2xl text-2xl cursor-pointer
+                             text-black backdrop-blur-sm border border-gray-400 group-hover:border-blue-500/30 transition duration-500
+                             rounded-full py-4 px-10">
+                Button
+            </div>
+        </div>
+    );
+}
