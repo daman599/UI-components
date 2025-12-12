89280bacd0e143f8a89df48fd9b29d359db58f7e
daman599 <damanpreetk117@gmail.com>
2025-12-12 16:23:10 +0530

    few improvements

--- ui-components/GlowElement.tsx
+++ ui-components/GlowElement.tsx
@@ -1,11 +1,11 @@
 export default function GlowElement() {
     return (
         <div className="relative group flex items-center">
-            <div className="absolute bg-blue-400 m-auto inset-0 rounded-full blur-md w-24 h-8
-                            group-hover:scale-150 transition duration-500" />
+            <div className="absolute bg-blue-400 m-auto inset-0 rounded-full blur-md scale-75 w-24 h-8
+                            group-hover:scale-150 transition-all duration-500" />
 
             <div className="relative shadow-xl text-2xl cursor-pointer
-                             text-black backdrop-blur-sm border border-gray-300 group-hover:border-blue-500/30 transition duration-500
+                             text-black backdrop-blur-sm border border-gray-300 group-hover:border-blue-500/20 transition-all duration-500
                              rounded-full py-4 px-10">
                 Button
             </div>
