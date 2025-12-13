150e1a1d1a5c3fe613c561cc6d76ed26ac137868
daman599 <damanpreetk117@gmail.com>
2025-12-13 15:15:35 +0530

    confetti effect added using lib

--- ui-components/LikeElement.tsx
+++ ui-components/LikeElement.tsx
@@ -3,6 +3,7 @@
 import { Heart } from "lucide-react";
 import { motion } from "motion/react";
 import { useState } from "react";
+import Confetti from "react-confetti-boom";
 
 export default function LikeElement() {
     const [clicked, setClicked] = useState<boolean>(false);
@@ -10,9 +11,20 @@ export default function LikeElement() {
     return (
         <motion.div onClick={() => setClicked(!clicked)}
             whileTap={{ scale: !clicked ? 1.2 : 1 }}
-            transition={{ type: "spring", damping: 10, stiffness: 150 }}
+            transition={{ type: "spring", damping: 5, stiffness: 150 }}
             className="cursor-pointer">
-            <Heart size={50} fill={`${clicked ? "#ee2677" : "white"}`} stroke="#2f243a" strokeWidth={1.2} />
+
+            {clicked && <Confetti
+                colors={['#ff577f', '#ff884b', '#ffd384', '#fff9b0']}
+                y={0}
+            />}
+
+            <Heart size={50}
+                fill={`${clicked ? "#ee2677" : "white"}`}
+                stroke="#2f243a"
+                strokeWidth={1.2}
+                className="relative"
+            />
         </motion.div>
     );
 }
