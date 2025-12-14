5539861f86823b8a0555ccd6a39ab4498b2a73a9
daman599 <damanpreetk117@gmail.com>
2025-12-14 14:08:38 +0530

    new component added that is avatars stack

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,9 @@
+import AvatarStack from "@/ui-components/AvatarStack";
+
 export default function Home() {
   return (
     <div className="flex items-center justify-center h-screen p-8 pb-20 sm:p-50 overflow-hidden">
+      <AvatarStack />
     </div >
   );
 }
---
+++ ui-components/AvatarStack.tsx
@@ -0,0 +1,34 @@
+"use client"
+
+import Image from "next/image";
+import { motion } from "motion/react";
+
+const avatars = [
+    { path: "/Avatar5.jpeg", z: 50 },
+    { path: "/Avatar2.jpeg", z: 40 },
+    { path: "/Avatar3.jpeg", z: 30 },
+    { path: "/Avatar1.jpeg", z: 20 },
+    { path: "/Avatar4.jpeg", z: 10 },
+];
+
+export default function AvatarStack() {
+    return (
+        <div className="flex items-center justify-center -space-x-20">
+            {avatars.map((avatar, i) => (
+                <motion.div
+                    initial={{ filter: "blur(1px)" }}
+                    whileHover={{ y: -50, filter: "blur(0px)" }}
+                    transition={{ ease: "easeInOut" }}
+                    key={i} className={`size-40 cursor-pointer rounded-full relative bg-black overflow-hidden `}
+                    style={{ zIndex: avatar.z }}>
+                    <Image src={avatar.path}
+                        fill
+                        alt={"Avatar Image"}
+                        className="object-contain"
+                    />
+                </motion.div>
+            ))
+            }
+        </div >
+    );
+}
